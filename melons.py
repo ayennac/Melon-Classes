a4c7df0789b7efd829dac5903c16d5323db38229
Jessica Dea <jessicardea@gmail.com>
2022-08-18 15:40:55 -0700

    International and Domestic Melon Order Classes

--- melons.py
+++ melons.py
@@ -1,62 +1,62 @@
 """Classes for melon orders."""
 
 
-class DomesticMelonOrder:
-    """A melon order within the USA."""
+# class DomesticMelonOrder:
+#     """A melon order within the USA."""
 
-    def __init__(self, species, qty):
-        """Initialize melon order attributes."""
+#     def __init__(self, species, qty):
+#         """Initialize melon order attributes."""
 
-        self.species = species
-        self.qty = qty
-        self.shipped = False
-        self.order_type = "domestic"
-        self.tax = 0.08
+#         self.species = species
+#         self.qty = qty
+#         self.shipped = False
+#         self.order_type = "domestic"
+#         self.tax = 0.08
 
-    def get_total(self):
-        """Calculate price, including tax."""
+#     def get_total(self):
+#         """Calculate price, including tax."""
 
-        base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
+#         base_price = 5
+#         total = (1 + self.tax) * self.qty * base_price
 
-        return total
+#         return total
 
-    def mark_shipped(self):
-        """Record the fact than an order has been shipped."""
+#     def mark_shipped(self):
+#         """Record the fact than an order has been shipped."""
 
-        self.shipped = True
+#         self.shipped = True
 
 
-class InternationalMelonOrder:
-    """An international (non-US) melon order."""
+# class InternationalMelonOrder:
+#     """An international (non-US) melon order."""
 
-    def __init__(self, species, qty, country_code):
-        """Initialize melon order attributes."""
+#     def __init__(self, species, qty, country_code):
+#         """Initialize melon order attributes."""
 
-        self.species = species
-        self.qty = qty
-        self.country_code = country_code
-        self.shipped = False
-        self.order_type = "international"
-        self.tax = 0.17
+#         self.species = species
+#         self.qty = qty
+#         self.country_code = country_code
+#         self.shipped = False
+#         self.order_type = "international"
+#         self.tax = 0.17
 
-    def get_total(self):
-        """Calculate price, including tax."""
+#     def get_total(self):
+#         """Calculate price, including tax."""
 
-        base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
+#         base_price = 5
+#         total = (1 + self.tax) * self.qty * base_price
 
-        return total
+#         return total
 
-    def mark_shipped(self):
-        """Record the fact than an order has been shipped."""
+#     def mark_shipped(self):
+#         """Record the fact than an order has been shipped."""
 
-        self.shipped = True
+#         self.shipped = True
 
-    def get_country_code(self):
-        """Return the country code."""
+#     def get_country_code(self):
+#         """Return the country code."""
 
-        return self.country_code
+#         return self.country_code
 
 
 class AbstractMelonOrder:
@@ -65,6 +65,7 @@ class AbstractMelonOrder:
     def __init__(self, species, qty):
         self.species = species
         self.qty = qty
+        self.shipped = False
 
     def get_total(self):
         """Calculate price, including tax."""
@@ -80,7 +81,25 @@ class AbstractMelonOrder:
         self.shipped = True
 
 
-class DomesticMelonOrder:
+class DomesticMelonOrder(AbstractMelonOrder):
+    def __init__(self, species, qty):
+        """A domestic melon order"""
+
+        super().__init__(species, qty)
+        self.order_type = "domestic"
+        self.tax = 0.08
 
 
-class InternationalMelonOrder:
+class InternationalMelonOrder(AbstractMelonOrder):
+    def __init__(self, species, qty, country_code):
+        """An international (non-US) melon order."""
+
+        super().__init__(species, qty)
+        self.country_code = country_code
+        self.order_type = "international"
+        self.tax = 0.17
+
+    def get_country_code(self):
+        """Return the country code."""
+
+        return self.country_code

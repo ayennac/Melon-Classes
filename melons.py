1fa6346299a483b24e32f18d9f1c57f4a59bb368
Jessica Dea <jessicardea@gmail.com>
2022-08-18 15:23:44 -0700

    Class AbstractMelonOrder

--- melons.py
+++ melons.py
@@ -57,3 +57,30 @@ class InternationalMelonOrder:
         """Return the country code."""
 
         return self.country_code
+
+
+class AbstractMelonOrder:
+    """An abstract base class that other Melon Orders inherit from."""
+
+    def __init__(self, species, qty):
+        self.species = species
+        self.qty = qty
+
+    def get_total(self):
+        """Calculate price, including tax."""
+
+        base_price = 5
+        total = (1 + self.tax) * self.qty * base_price
+
+        return total
+
+    def mark_shipped(self):
+        """Record the fact than an order has been shipped."""
+
+        self.shipped = True
+
+
+class DomesticMelonOrder:
+
+
+class InternationalMelonOrder:

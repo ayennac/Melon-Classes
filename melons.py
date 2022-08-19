9621ee951e5fbc3f1be4be006c378dd475472d3e
Jessica Dea <jessicardea@gmail.com>
2022-08-18 17:07:03 -0700

    get_base_price function

--- melons.py
+++ melons.py
@@ -1,62 +1,7 @@
 """Classes for melon orders."""
 
-
-# class DomesticMelonOrder:
-#     """A melon order within the USA."""
-
-#     def __init__(self, species, qty):
-#         """Initialize melon order attributes."""
-
-#         self.species = species
-#         self.qty = qty
-#         self.shipped = False
-#         self.order_type = "domestic"
-#         self.tax = 0.08
-
-#     def get_total(self):
-#         """Calculate price, including tax."""
-
-#         base_price = 5
-#         total = (1 + self.tax) * self.qty * base_price
-
-#         return total
-
-#     def mark_shipped(self):
-#         """Record the fact than an order has been shipped."""
-
-#         self.shipped = True
-
-
-# class InternationalMelonOrder:
-#     """An international (non-US) melon order."""
-
-#     def __init__(self, species, qty, country_code):
-#         """Initialize melon order attributes."""
-
-#         self.species = species
-#         self.qty = qty
-#         self.country_code = country_code
-#         self.shipped = False
-#         self.order_type = "international"
-#         self.tax = 0.17
-
-#     def get_total(self):
-#         """Calculate price, including tax."""
-
-#         base_price = 5
-#         total = (1 + self.tax) * self.qty * base_price
-
-#         return total
-
-#     def mark_shipped(self):
-#         """Record the fact than an order has been shipped."""
-
-#         self.shipped = True
-
-#     def get_country_code(self):
-#         """Return the country code."""
-
-#         return self.country_code
+import random
+from datetime import datetime
 
 
 class AbstractMelonOrder:
@@ -66,15 +11,22 @@ class AbstractMelonOrder:
         self.species = species
         self.qty = qty
         self.shipped = False
+        self.base_price = 0
+        self.order_time = datetime.today()
+
+    def get_base_price(self):
+        self.base_price = random.randint(5, 9)
+
+        if self.order_time.weekday() in [0, 1, 2, 3, 4] and self.order_time.hour in [8, 9, 10]:
+            self.base_price += 4
 
     def get_total(self):
         """Calculate price, including tax."""
-        base_price = 5
 
         if self.species == "Christmas":
-            base_price = 9
+            self.base_price *= 1.5
 
-        total = (1 + self.tax) * self.qty * base_price
+        total = (1 + self.tax) * self.qty * self.base_price
 
         if self.order_type == "international" and self.qty < 10:
             total += 3
@@ -109,3 +61,17 @@ class InternationalMelonOrder(AbstractMelonOrder):
         """Return the country code."""
 
         return self.country_code
+
+
+class GovernmentMelonOrder(AbstractMelonOrder):
+    def __init__(self, species, qty):
+        """A government melon order"""
+
+        super().__init__(species, qty)
+        self.order_type = "government"
+        self.passed_inspection = False
+        self.tax = 0
+
+    def mark_inspection(self, passed):
+        if passed == True:
+            self.passed_inspection = True
